0bb49d388423f5c18258d0fca2150d119fa3b87b
Gary Olson <golsonmke@gmail.com>
2020-03-18 17:10:44 -0500

    Completed the Employee/Customer classes.

---
+++ TrashCollectorProject/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollectorProject.Models
+{
+    public class Customer
+    {
+        public int CutomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public int AcountBalance { get; set; }
+        public string WeeklyPickUpDay { get; set; }
+        public bool IsPickedUp { get; set; }
+
+        
+
+
+    }
+}
